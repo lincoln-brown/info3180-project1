40d1d92696e9e944036e86e64160591384876413
Lincoln-Brown <links1155@gmail.com>
2020-03-14 21:02:59 -0500

    fixed image size issue

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ UPLOAD_FOLDER = './app/static/photos'
 app = Flask(__name__)
 app.config.from_object(__name__)#allow to configura gobla variables  such as uploads_folder
 app.config['SECRET_KEY'] = 'Sup3r$3cretke#@#^26l#)('
-app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://dev:@Links1010@localhost/project"
+#app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://dev:@Links1010@localhost/project"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 db = SQLAlchemy(app)
 from app import views
